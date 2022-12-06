101fc16afbdf44eb524a3624a1b955e460bdb59c
bnnasrjz <113246083+bnnasrjz@users.noreply.github.com>
2022-12-06 11:39:00 +0100

    Add files via upload

---
+++ env.d.ts
@@ -0,0 +1,12 @@
+/// <reference/>
+
+/**
+ *  @external https://vitejs.dev/guide/env-and-mode.html
+ */
+ interface ImportMetaEnv {
+    readonly VITE_GOOGLE_MAPS_API_KEY: string;
+  }
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+ }
+  
